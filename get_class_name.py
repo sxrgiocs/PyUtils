a47ff2ac09e2aba4550470aa4279a1dd5db4fb5c
Sergio Carreras <46929392+sxrgiocs@users.noreply.github.com>
2023-06-21 13:59:26 +0200

    Update get_class_name.py

--- get_class_name.py
+++ get_class_name.py
@@ -1,6 +1,10 @@
 def get_class_name(myclass):
     """
     Return class name along with its non-private attributes with values.
+    This function works by inspecting the `__dict__` attribute of the class or
+    object and retrieving its non-private attributes (attributes not starting
+    with '_'). The attribute names and values are then formatted into a string
+    with the class name.
 
     Parameters
     ----------
@@ -11,13 +15,6 @@ def get_class_name(myclass):
     -------
     str
         The name of the class along with its non-private attrs with values.
-
-    Notes
-    -----
-    This function works by inspecting the `__dict__` attribute of the class or
-    object and retrieving its non-private attributes (attributes not starting
-    with '_'). The attribute names and values are then formatted into a string
-    with the class name.
     """
 
     args = ', '.join(f'{k}={v}' for k, v in myclass.__dict__.items()
